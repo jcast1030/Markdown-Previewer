1dadf7c8eb9f77ad575fe681a344285473f3482a
Joses <jc.thecr@gmail.com>
2023-06-18 23:17:51 -0500

    just css

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
+marked.setOptions({
+    breaks: true,
+});
+
+const renderer = new marked.Renderer();
+
 function App(){
 
+    const [text, setText] = React.useState("");
+
     return (
-        <div>Hello world</div>
+        <div className="text-center px-4">
+            <h1 className="p-4">Markdown Previewer</h1>
+            <textarea
+                name="text"
+                id="text"
+                rows="10"
+                onChange={(e) => setText(e.target.value)}
+                value={text}
+                className="textarea">
+            </textarea>
+            <h3 className="mt-3">Output</h3>
+            <Preview markdown={text} />
+        </div>
+    );
+}
+
+function Preview({markdown}) {
+    return (
+        <div
+            dangerouslySetInnerHTML={{
+                __html: marked(markdown, { renderer: renderer})
+            }}
+            id="preview"
+        >
+
+        </div>
     );
 }
 
